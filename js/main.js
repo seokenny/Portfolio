a8040398e7a3ffaf61b1801a6db822738b2d4b05
Kenny Seo <44145254+seokenny@users.noreply.github.com>
2020-04-14 19:43:00 -0400

    Added animations and social buttons

--- js/main.js
+++ js/main.js
@@ -24,3 +24,58 @@ $(".wcc__project").on("click",function(){
     $(".wt__project").removeClass("inactive");
     window.scrollTo(0, 0);
 });
+
+// WorkList
+$(".worklist__1").on("click",function(){
+    $(".worklist__1").addClass("active");
+    for(var i = 0; i < 7; i++){
+        if(document.querySelector(".worklist__" + i) != document.querySelector(".worklist__1")){
+            $(".worklist__" + i).removeClass("active");
+        }
+    }
+});
+
+$(".worklist__2").on("click",function(){
+    $(".worklist__2").addClass("active");
+    for(var i = 0; i < 7; i++){
+        if(document.querySelector(".worklist__" + i) != document.querySelector(".worklist__2")){
+            $(".worklist__" + i).removeClass("active");
+        }
+    }
+});
+
+$(".worklist__3").on("click",function(){
+    $(".worklist__3").addClass("active");
+    for(var i = 0; i < 7; i++){
+        if(document.querySelector(".worklist__" + i) != document.querySelector(".worklist__3")){
+            $(".worklist__" + i).removeClass("active");
+        }
+    }
+});
+
+$(".worklist__4").on("click",function(){
+    $(".worklist__4").addClass("active");
+    for(var i = 0; i < 7; i++){
+        if(document.querySelector(".worklist__" + i) != document.querySelector(".worklist__4")){
+            $(".worklist__" + i).removeClass("active");
+        }
+    }
+});
+
+$(".worklist__5").on("click",function(){
+    $(".worklist__5").addClass("active");
+    for(var i = 0; i < 7; i++){
+        if(document.querySelector(".worklist__" + i) != document.querySelector(".worklist__5")){
+            $(".worklist__" + i).removeClass("active");
+        }
+    }
+});
+
+$(".worklist__6").on("click",function(){
+    $(".worklist__6").addClass("active");
+    for(var i = 0; i < 7; i++){
+        if(document.querySelector(".worklist__" + i) != document.querySelector(".worklist__6")){
+            $(".worklist__" + i).removeClass("active");
+        }
+    }
+});
